bfe5b63403a3ed764c5ca5acdcae1a686acc7ae4
まさ <zenchin207@gmail.com>
2021-02-02 02:40:02 +0900

    [update]問題追加

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-// 問題文
-const question = "ドラゴンボールに出てくる主人公の名前は次のうちどれ？";
-// 選択欄
-const answers = ["孫悟空", "孫悟飯", "ブロリー", "ベジータ"];
-// 答え
-const correct = "孫悟空";
+const quiz = [
+  {
+    question: "ドラゴンボールに出てくる主人公の名前は次のうちどれ？", // 問題文
+    answers: ["孫悟空", "孫悟飯", "ブロリー", "ベジータ"], // 選択欄
+    correct: "孫悟空", // 答え
+  },
+  {
+    question: "ジョジョの奇妙な冒険３部の主人公は次のうちどれ？",
+    answers: [
+      "ジョセフ・ジョースター",
+      "東方仗助",
+      "空条承太郎",
+      "ジョルノ・ジョバーナ",
+    ],
+    correct: "空条承太郎",
+  },
+  {
+    question: "青森県の生産量が一位の物は次のうちどれ？",
+    answers: ["絆創膏", "靴下", "包帯", "ストッキング"],
+    correct: "ストッキング",
+  },
+];
+
+const quizLength = quiz.length;
+let quizIndex = 0; //constだと++できないのでエラーになる
+let score = 0;
 
 const $button = document.getElementsByTagName("button");
 // ボタンの数を数える変数
@@ -11,10 +31,10 @@ let buttonLength = $button.length;
 
 // クイズの問題、選択肢を定義
 const setupQuiz = () => {
-  document.getElementById("js-question").textContent = question;
+  document.getElementById("js-question").textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
   while (buttonIndex < buttonLength) {
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 };
@@ -22,17 +42,26 @@ const setupQuiz = () => {
 setupQuiz();
 
 const clickHandler = (e) => {
-  if (correct === e.target.textContent) {
+  if (quiz[quizIndex].correct === e.target.textContent) {
     window.alert("正解!");
+    score++;
   } else {
     window.alert("不正解");
+  }
+
+  quizIndex++;
+
+  if (quizIndex < quizLength) {
+    setupQuiz();
+  } else {
+    window.alert("終了！あなたの正解数は" + score + "/" + quizLength + "です");
   }
 };
 
 let handlerIndex = 0;
 while (handlerIndex < buttonLength) {
   $button[handlerIndex].addEventListener("click", (e) => {
-    clickHandler(e)
-  });  
-    handlerIndex++;
+    clickHandler(e);
+  });
+  handlerIndex++;
 }
