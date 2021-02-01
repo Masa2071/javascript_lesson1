4d7ad3452bd43e8ac9df5e4bb260ec0df8587531
まさ <zenchin207@gmail.com>
2021-02-01 18:36:34 +0900

    [update]リファクタリング

--- app.js
+++ app.js
@@ -5,41 +5,34 @@ const answers = ["孫悟空", "孫悟飯", "ブロリー", "ベジータ"];
 // 答え
 const correct = "孫悟空";
 
-document.getElementById("js-question").textContent = question;
+const $button = document.getElementsByTagName("button");
+// ボタンの数を数える変数
+let buttonLength = $button.length;
 
-document.getElementsByTagName("button")[0].textContent = answers[0];
-document.getElementsByTagName("button")[1].textContent = answers[1];
-document.getElementsByTagName("button")[2].textContent = answers[2];
-document.getElementsByTagName("button")[3].textContent = answers[3];
+// クイズの問題、選択肢を定義
+const setupQuiz = () => {
+  document.getElementById("js-question").textContent = question;
+  let buttonIndex = 0;
+  while (buttonIndex < buttonLength) {
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+  }
+};
 
-document.getElementsByTagName("button")[0].addEventListener("click", () => {
-  if (correct === document.getElementsByTagName("button")[0].textContent) {
+setupQuiz();
+
+const clickHandler = (e) => {
+  if (correct === e.target.textContent) {
     window.alert("正解!");
   } else {
     window.alert("不正解");
   }
-});
+};
 
-document.getElementsByTagName("button")[1].addEventListener("click", () => {
-  if (correct === document.getElementsByTagName("button")[1].textContent) {
-    window.alert("正解!");
-  } else {
-    window.alert("不正解");
-  }
-});
-
-document.getElementsByTagName("button")[2].addEventListener("click", () => {
-  if (correct === document.getElementsByTagName("button")[2].textContent) {
-    window.alert("正解!");
-  } else {
-    window.alert("不正解");
-  }
-});
-
-document.getElementsByTagName("button")[3].addEventListener("click", () => {
-  if (correct === document.getElementsByTagName("button")[3].textContent) {
-    window.alert("正解!");
-  } else {
-    window.alert("不正解");
-  }
-});
+let handlerIndex = 0;
+while (handlerIndex < buttonLength) {
+  $button[handlerIndex].addEventListener("click", (e) => {
+    clickHandler(e)
+  });  
+    handlerIndex++;
+}
